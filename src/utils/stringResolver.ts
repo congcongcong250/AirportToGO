d4a4353f9a88a1ecf0b94ce555b2d44a20100663
leewang <lirenxn@gmail.com>
2019-09-17 13:57:00 +1000

    Update city name resolver

--- src/utils/stringResolver.ts
+++ src/utils/stringResolver.ts
@@ -2,6 +2,6 @@ import { CityModel } from '../data/models';
 
 export const resolveCityName = (city: CityModel) => {
   // default value as guard in case there is unfound dirty data
-  const { timeZoneName = '', cityName = '' } = city;
-  return `${timeZoneName.split('/')[1]} ${cityName}`;
+  const { cityName = '', cityCode = '' } = city;
+  return `${cityName}(${cityCode})`;
 };
