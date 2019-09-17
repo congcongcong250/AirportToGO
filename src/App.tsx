00c6b42a6ec1cad22b2b51f7026a84573c0ed632
leewang <lirenxn@gmail.com>
2019-09-17 21:33:37 +1000

    Improve AppShell structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
-import AppRouter from './AppRouter';
+import { Route, Link } from 'react-router-dom';
+import AirportFeedContainer from './components/airport-feed-container';
+import AirportDetailContainer from './components/airport-detail-container';
 
 const App: React.FC = () => {
-  return <AppRouter />;
+  return (
+    <div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/feed">Airports</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/feed" exact component={AirportFeedContainer} />
+        <Route path="/feed/:airportCode" exact component={AirportDetailContainer} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
