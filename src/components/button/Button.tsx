f608863218f7f734deb9454bbb69311d60a53168
leewang <lirenxn@gmail.com>
2019-09-18 02:19:13 +1000

    butter button style

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,15 +3,23 @@ import { StyledButton } from './styles';
 
 interface ButtonProps {
   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  disabled?: boolean;
 }
 class Button extends React.PureComponent<ButtonProps> {
   render() {
-    const { children } = this.props;
-    return <StyledButton onClick={this.handleButtonOnClick}>{children}</StyledButton>;
+    const { children, disabled } = this.props;
+    return (
+      <StyledButton disabled={disabled} onClick={this.handleButtonOnClick}>
+        {children}
+      </StyledButton>
+    );
   }
 
   private handleButtonOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const { onClick } = this.props;
+    const { onClick, disabled } = this.props;
+    if (disabled) {
+      return;
+    }
     onClick && onClick(e);
   };
 }
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -6,15 +6,26 @@ export const StyledButton = styled.button`
   width: 100%;
   max-width: 200px;
   border-radius: 5px;
-  background-color: ${COLORS.main};
+  background-color: ${props => {
+    if (!props.disabled) {
+      return COLORS.main;
+    } else {
+      return COLORS.lightGrey;
+    }
+  }};
   box-shadow: 0.5px 2px 3px 0px ${COLORS.shadow};
   height: 40px;
   color: white;
-  :hover {
-    transition: background-color 0.15s ease-in-out;
-    cursor: pointer;
-    background-color: ${COLORS.lightRed};
-  }
+  ${props => {
+    if (!props.disabled) {
+      return `:hover {
+        transition: background-color 0.15s ease-in-out;
+        cursor: pointer;
+        background-color: ${COLORS.lightRed};
+      }`;
+    }
+  }}
+
   @media (max-width: 768px) {
     height: 20px;
   }
