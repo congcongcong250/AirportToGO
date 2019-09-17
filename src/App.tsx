81440f2bca59f6bec4f32f02325f562378b31bfd
leewang <lirenxn@gmail.com>
2019-09-18 02:13:33 +1000

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import AirportFeedContainer from './components/airport-feed-container';
 import AirportDetailContainer from './components/airport-detail-container';
+import Button from './components/button';
 
 const App: React.FC = () => {
   return (
     <div>
       <div>
         <nav>
-          <ul>
-            <li>
-              <Link to="/feed">Airports</Link>
-            </li>
-          </ul>
+          <Button>
+            <Link to="/feed">Airports</Link>
+          </Button>
         </nav>
-
+        <Route path="/" exact component={AirportFeedContainer} />
         <Route path="/feed" exact component={AirportFeedContainer} />
         <Route path="/feed/:airportCode" exact component={AirportDetailContainer} />
       </div>
